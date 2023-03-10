b5a35c6d7c717a35b04c999b5879fd0e039c375f
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-03-10 20:42:47 +0700

    akhirnya index.js setelah nyari lagi rumus hehehe. alhamdulillah ...

--- index.js
+++ index.js
@@ -1,79 +1,38 @@
-// import PersamaanNonLinier from './PersamaanNonLinier';
+const bisection = ({ f, a, b, e }, N = 1, results = []) => {
+  const c = (a+b) / 2;
+  const bMinC = b - c;
+  const fb = f(b);
+  const fc = f(c);
 
-// const persamaanNonLinier = new PersamaanNonLinier({
-//   f: (x) => (x**5)-3(x**3)+2(x**2)+7,
-//   a: -2,
-//   b: 4,
-//   epsilon: 0.0001,
-// });
+  resultPerIteration = {
+    iterasi: N,
+    a,
+    b,
+    'b - c': bMinC,
+    'f(b)': fb,
+    'f(c)': fc,
+  };
 
-// persamaanNonLinier.bisect();
-
-const bisect = ({
-  f,
-  a,
-  b,
-  epsilon,
-}, countStart = 1) => {
-  let akar = null;
-
-  const c = (a + b)/ 2;
-
-  if (b - c < epsilon) {
-    console.log('b - c', b - c);
-    akar = c;
-    return console.log({
-      iterasi: countStart,
-      a: a,
-      b: b,
-      c: c,
-      functionB: f(b),
-      functionC: f(c),
-      functionBxFunctionC: (f(b)*f(c)),
-      keterangan: `akar ketemu: ${akar}`,
-    });
-  } else if (b - c >= epsilon) {
-    console.log('b - c', b - c);
-    return console.log({
-      iterasi: countStart,
-      a: a,
-      b: b,
-      c: c,
-      functionB: f(b),
-      functionC: f(c),
-      functionBxFunctionC: (f(b)*f(c)),
-      keterangan: 'Titik yang dicari',
-    });
+  
+  if (b - c <= e) {
+    resultPerIteration.keterangan = 'Titik yang dicari';
+    results.push(resultPerIteration);
+    return console.log(results);
   }
+  
+  resultPerIteration.keterangan = 'Bukan titik yang dicari';
+  results.push(resultPerIteration);
 
-  console.log({
-    iterasi: countStart,
-    a: a,
-    b: b,
-    c: c,
-    functionB: f(b),
-    functionC: f(c),
-    functionBxFunctionC: (f(b)*f(c)),
-    keterangan: 'Bukan titik yang dicari',
-  });
-
-  f(b) * f(c) <= 0 ? a = c : b = c;
-
-  bisect({ f, a, b, c }, countStart + 1)
+  f(b)*f(c) <= 0
+  ? a = c 
+  : b = c;
+  
+  bisection({ f, a, b, e }, N + 1, results);
 };
 
-console.log('coba1');
-bisect({
+bisection({
   f: (x) => (x**5)-3*(x**3)+2*(x**2)+7,
   a: -2,
   b: 4,
-  epsilon: 0.0001,
-});
-
-console.log('coba2');
-bisect({
-  f: (x) => (x**2)+x-6,
-  a: -1,
-  b: 4,
-  epsilon: 0.0001,
+  e: 0.0001,
 });
